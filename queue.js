ddd4811fe429537ae2cb089c6b1814e7c1d53d4b
Karly Stark <karlystark@gmail.com>
2023-10-02 16:09:54 -0700

    queue all tests passing

--- queue.js
+++ queue.js
@@ -8,7 +8,7 @@ class Node {
     this.val = val;
   }
 }
-
+//linked list!
 /** Queue: chained-together nodes where you can
  *  remove from the front or add to the back. */
 
@@ -20,26 +20,50 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    let newNode = new Node(val);
 
+    if(this.first === null) this.first = newNode;
+
+    if(this.last !== null) this.last.next = newNode;
+
+    this.last = newNode;
+
+    this.size++;
+
+    return undefined;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
-
+    if(this.size === 0){
+      throw new Error;
+    } else if (this.first === this.last){
+      let res = this.first.val;
+      this.first = null;
+      this.last = null;
+      this.size--;
+      return res;
+    } else {
+      let res = this.first.val;
+      this.first = this.first.next;
+      this.size--;
+      return res;
+    }
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
+    return this.first.val;
 
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0? true : false;
   }
 }
 
