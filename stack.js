90869388a232400bdcb3c54c0a3c5ae404c68ba6
Karly Stark <karlystark@gmail.com>
2023-10-02 16:15:02 -0700

    stack all tests passing

--- stack.js
+++ stack.js
@@ -19,6 +19,19 @@ class Stack {
   /** push(val): add new value to the top of the stack. Returns undefined. */
 
   push(val) {
+    let newNode  = new Node(val);
+
+    if(this.top === null){
+      newNode.next === null;
+    } else {
+      newNode.next = this.top;
+    }
+
+    this.top = newNode;
+
+    this.size++;
+
+    return undefined;
 
   }
 
@@ -26,19 +39,27 @@ class Stack {
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    if(this.top === null){
+      throw new Error;
+    } else {
+      let res = this.top.val;
+      this.top = this.top.next;
+      this.size--;
+      return res;
+    }
 
   }
 
   /** peek(): return the value of the top node in the stack. */
 
   peek() {
-
+    return this.top.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0? true: false;
   }
 }
 
